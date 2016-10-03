5e9bf5c7967e053899a6e40b25174e78dbf090a5
Max <multi3volume@gmail.com>
2016-10-03 14:49:28 +0300

    Constants and input data for stage 1 added.

---
+++ src/main/java/app/Const.java
@@ -0,0 +1,15 @@
+package app;
+
+
+public class Const {
+
+	// Мольная масса элементов (кг/моль)
+	// Эти данные взяты из переодичесой системы Д.И.Менделеева
+	public static final double MolarMassAl = 0.02698;
+	public static final double MolarMassC = 0.01201;
+	public static final double MolarMassFe = 0.055847;
+	public static final double MolarMassSi = 0.0280855;
+	public static final double MolarMassTi = 0.04788;
+	public static final double MolarMassMn = 0.054938;
+
+}
