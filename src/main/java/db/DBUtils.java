4cc1917cc9bcfae76fe65a0e9808c5972e17e017
Max <multi3volume@gmail.com>
2016-12-07 07:22:42 +0200

    Add util class for work with db. Add db columns, main table name to this class.

---
+++ src/main/java/db/DBUtils.java
@@ -0,0 +1,37 @@
+package db;
+
+public class DBUtils {
+
+	private static final String DB_NAME = "fmps.db";
+	public static final String TABLE_FMPS_MAIN = "fmps_main";
+	public static final String DLC = ", ";
+
+	// Columns
+	public static final String LAST_NAME = "lastname";
+	public static final String FIRST_NAME = "firstname";
+	public static final String MIDDLE_NAME = "middlename";
+	public static final String ST1_PROGRESS = "st1_progress";
+	public static final String ST2_PROGRESS = "st2_progress";
+	public static final String ST3_PROGRESS = "st3_progress";
+	public static final String ST1_FE = "st1_fe";
+	public static final String ST1_C = "st1_c";
+	public static final String ST1_MN = "st1_mn";
+	public static final String ST1_P_ENV = "st1_p_env";
+	public static final String ST1_F_SURF_WELD_AREA = "st1_f_surface_area";
+	public static final String ST1_MP_WEIGHT_MOLTEN_METAL = "st1_mp_weight_molten_metal";
+	public static final String ST1_TEMPERATURE = "st1_temperature";
+	public static final String ST1_TIME = "st1_time";
+	public static final String ST2_ARGON = "st2_argon";
+	public static final String ST2_CO2 = "st2_co2";
+	public static final String ST2_O2 = "st2_o2";
+	public static final String ST2_CO = "st2_co";
+	public static final String ST2_O = "st2_o";
+	public static final String ST2_C = "st2_c";
+	public static final String ST2_TEMPERATURE = "st2_temperature";
+	public static final String ST3_CO_PARTIAL_PRESSURE = "st3_co_partial_pressure";
+	public static final String ST3_O_PARTIAL_PRESSURE = "st3_o_partial_pressure";
+	public static final String ST3_O_DISSOLVE = "st3_dissolve";
+	public static final String ST3_ATM_PRESSURE = "st3_atm_pressure";
+	public static final String ST3_TEMPERATURE = "st3_temperature";
+
+}
