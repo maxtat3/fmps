42f3d24e98c69cc6e26847ab5ffbae15b4e1b91d
Max <multi3volume@gmail.com>
2016-12-13 12:27:53 +0200

    Simplify app run - create only calculator class.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,32 +1,11 @@
 package app;
 
 
-import ui.Stage1Question1Frame;
+import stage1.calc.Calc;
 
-import javax.swing.*;
 
 public class App {
 	public static void main(String[] args) {
-
-		Stage1 stage1 = new Stage1();
-
-		double checkAllElemSum = stage1.checkAllElementsSum();
-		System.out.println("checkAllElemSum = " + checkAllElemSum);
-
-		double[] moles = stage1.moleFractionAllElements();
-		for (double mole : moles) {
-			System.out.println("moles = " + mole * 100);
-		}
-
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new App().startUI();
-			}
-		});
-	}
-
-	private void startUI() {
-		new Stage1Question1Frame();
+		new Calc();
 	}
 }
